529364921ae08feeeb7d49a542a080baf406705c
Daniel Apodaca <daniela@zoomcatalog.com>
2022-06-25 07:10:40 -0600

    display currently selected workspace

--- src/components/workspaces/WorkspacesPopover.vue
+++ src/components/workspaces/WorkspacesPopover.vue
@@ -1,56 +1,74 @@
 <script setup lang="ts">
 import { useWorkspace, workspaceCategoryHelpers } from '../../composables'
-import { useUpdateLastUsedWorkspaceIdMutation } from '../../services/useUserService';
-import { errorHandler } from '../../utils/errorHandler';
+import { useUpdateLastUsedWorkspaceIdMutation } from '../../services/useUserService'
+import { errorHandler } from '../../utils/errorHandler'
 import { DPopover, DButton } from '../primitives'
+import { PencilIcon, ChevronDownIcon } from '@heroicons/vue/outline'
 
 const updateLastUsedWorkspaceId = useUpdateLastUsedWorkspaceIdMutation()
 const [workspaceOptions, workspaceHandlers] = useWorkspace({
   handlers: {
     async onSwitchWorkspace(workspace) {
       try {
-        await updateLastUsedWorkspaceId.mutateAsync({ workspaceId: workspace.id })
+        await updateLastUsedWorkspaceId.mutateAsync({
+          workspaceId: workspace.id,
+        })
       } catch (err) {
-        errorHandler(err)        
+        errorHandler(err)
       }
-    }
-  }
+    },
+  },
 })
 </script>
 
 <template>
   <DPopover>
-    <DButton fullWidth>Workspaces</DButton>
+    <DButton fullWidth>
+      <div class="flex flex-col items-start w-full py-2">
+        {{workspaceOptions.workspace?.title}}
+        <span class="text-sm font-normal">
+          {{ `${workspaceOptions.workspace?.category} workspace` }}
+        </span>
+      </div>
+      <ChevronDownIcon class="w-4 h-4" />
+    </DButton>
     <template #content="{ close }">
       <div class="z-10 m-2 w-screen max-w-sm">
         <div
           class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5"
         >
-          <div class="relative flex flex-col gap-8 bg-white p-7">
-            <router-link
+          <div class="relative flex flex-col gap-4 bg-white p-7">
+            <div
               v-for="workspace in workspaceOptions.workspaces"
               :key="workspace.id"
-              @click="close(); workspaceHandlers.switchWorkspace(workspace, false)"
-              :to="`/w/${workspace.id}/${workspace.category}`"
-              class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+              class="flex items-center gap-4"
             >
-              <div
-                class="bg-blue-100 rounded-lg flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12"
+              <router-link
+                @click="close(); workspaceHandlers.switchWorkspace(workspace, false)"
+                :to="`/w/${workspace.id}/${workspace.category}`"
+                class="-m-3 w-full flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-50"
               >
-                <component
-                  :is="workspaceCategoryHelpers[workspace.category].icon"
-                  class="w-6 h-6 text-blue-600"
-                />
-              </div>
-              <div class="ml-4">
-                <p class="text-sm font-medium text-gray-900">
-                  {{ workspace.title }}
-                </p>
-                <p class="text-sm text-gray-500">
-                  {{ `${workspace.title} workspace` }}
-                </p>
-              </div>
-            </router-link>
+                <div
+                  class="bg-blue-100 rounded-lg flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12"
+                >
+                  <component
+                    :is="workspaceCategoryHelpers[workspace.category].icon"
+                    class="w-6 h-6 text-blue-600"
+                  />
+                </div>
+                <div class="ml-4">
+                  <p class="text-sm font-medium text-gray-900">
+                    {{ workspace.title }}
+                  </p>
+                  <p class="text-sm text-gray-500">
+                    {{ `${workspace.category} workspace` }}
+                  </p>
+                </div>
+              </router-link>
+              <button class="w-6 h-16">
+                <PencilIcon class="w-5 h-5 text-slate-900" />
+              </button>
+            </div>
           </div>
           <div class="bg-gray-50 p-4">
             <router-link
