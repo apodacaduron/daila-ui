a6c19c04d30183c4db41b43a3ce54d9bc4539357
Daniel Apodaca <daniela@zoomcatalog.com>
2022-07-16 10:54:26 -0600

    dialog dark theme improvement

--- src/components/primitives/Dialog/Dialog.vue
+++ src/components/primitives/Dialog/Dialog.vue
@@ -12,7 +12,7 @@ interface Props {
 }
 defineProps<Props>()
 defineEmits<{
-  'close': (close: boolean) => void
+  close: (close: boolean) => void
 }>()
 </script>
 
@@ -44,9 +44,7 @@ defineEmits<{
             leave-from="opacity-100 scale-100"
             leave-to="opacity-0 scale-95"
           >
-            <DialogPanel
-              class="dialog__panel"
-            >
+            <DialogPanel class="dialog__panel">
               <DialogTitle
                 as="h3"
                 class="text-lg font-medium leading-6 text-gray-900"
@@ -72,7 +70,7 @@ defineEmits<{
 .dialog {
   @apply relative z-10;
   &__background {
-    @apply fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm;
+    @apply fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm dark:bg-opacity-60;
   }
   &__panel {
     @apply w-full max-w-md transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all;
