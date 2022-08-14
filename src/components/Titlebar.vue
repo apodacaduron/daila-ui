298ee6fe4710b9fcfbf115447be93788b569af1f
Daniel Apodaca <daniela@zoomcatalog.com>
2022-08-14 08:51:51 -0600

    titlebar component

---
+++ src/components/Titlebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="titlebar">
+    <div class="titlebar__text">
+      <h1><slot name="title" /></h1>
+      <p><slot name="subtitle" /></p>
+    </div>
+    <div class="titlebar__actions">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.titlebar {
+  @apply w-full mb-4;
+  @apply flex items-center justify-between;
+  &__text {
+    h1 {
+      @apply text-3xl flex items-center gap-3;
+    }
+    p {
+      @apply text-slate-500;
+    }
+  }
+}
+</style>
--- src/pages/workspaces/psychologist/TeamMembers.vue
+++ src/pages/workspaces/psychologist/TeamMembers.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { DButton } from '../../../components/ui'
 import { TeamMembersTable } from '../../../features/team-members'
+import Titlebar from '../../../components/Titlebar.vue'
 </script>
 
 <template>
   <div class="team-members">
+    <Titlebar>
+      <template #title>Team members</template>
+      <template #subtitle>Manage your team members</template>
+      <template #actions><DButton>+ Add new team member</DButton></template>
+    </Titlebar>
     <TeamMembersTable />
   </div>
 </template>
