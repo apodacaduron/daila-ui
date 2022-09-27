afa425ac5cd306205a4320a5c86c6a1c3416ed8f
Daniel Apodaca <apodacaduron@gmail.com>
2022-09-27 08:19:42 -0600

    basic sign in and sign up pages

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,6 +1,14 @@
 import type { NextPage } from 'next'
+import { useForm } from '@mantine/form';
 
 const SignIn: NextPage = () => {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  })
+
   return <div>Sign in</div>
 }
 
