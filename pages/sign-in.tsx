c6bd606ab202a65156627337f1390b20f6ef4570
Daniel Apodaca <daniela@zoomcatalog.com>
2022-10-01 18:23:16 -0600

    remove unused import

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
 import toast from 'react-hot-toast';
 import { FcGoogle } from 'react-icons/fc';
